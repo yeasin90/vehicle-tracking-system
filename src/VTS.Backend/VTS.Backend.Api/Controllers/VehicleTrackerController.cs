5a46b6b7b370e12efb24026af648eacbcc3354d6
Yeasin Abedin <abedinyeasin@gmail.com>
2021-12-16 01:44:03 +0600

    Added controller

---
+++ src/VTS.Backend/VTS.Backend.Api/Controllers/VehicleTrackerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace VTS.Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class VehicleTrackerController : ControllerBase
+    {
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult> RegisterDeviceAsync()
+        {
+            return Ok();
+        }
+    }
+}
