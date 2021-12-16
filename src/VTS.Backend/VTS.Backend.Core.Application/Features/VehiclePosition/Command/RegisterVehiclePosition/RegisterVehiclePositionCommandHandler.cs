2c7a296a102b1074eab09587f8f06f0e6bfda524
Yeasin Abedin <abedinyeasin@gmail.com>
2021-12-16 11:43:58 +0600

    Validation in RegisterVehiclePositionCommandHandler

--- src/VTS.Backend/VTS.Backend.Core.Application/Features/VehiclePosition/Command/RegisterVehiclePosition/RegisterVehiclePositionCommandHandler.cs
+++ src/VTS.Backend/VTS.Backend.Core.Application/Features/VehiclePosition/Command/RegisterVehiclePosition/RegisterVehiclePositionCommandHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
 using VTS.Backend.Core.Application.Contracts;
+using VTS.Backend.Core.Application.Exceptions;
 
 namespace VTS.Backend.Core.Application.Features.VehiclePosition.Command.RegisterVehiclePosition
 {
@@ -19,6 +20,9 @@ namespace VTS.Backend.Core.Application.Features.VehiclePosition.Command.Register
 
         public async Task<VehiclePositionDto> Handle(RegisterVehiclePositionCommand request, CancellationToken cancellationToken)
         {
+            if (request.Latitude == 0 || request.Longitude == 0)
+                throw new AppException($"{nameof(request.Latitude)} or {nameof(request.Longitude)} cannot be zero");
+
             var entity = _mapper.Map<Domain.Entities.VehiclePosition>(request);
             var savedEntity = await _vehiclePositionRepository.AddAsync(entity);
             return _mapper.Map<VehiclePositionDto>(savedEntity);
