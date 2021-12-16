983c845be522a7fa9fb0c11c7f0f8553e01d89aa
Yeasin Abedin <abedinyeasin@gmail.com>
2021-12-16 14:33:05 +0600

    Added extenion method for datetime to timestamp conversion

---
+++ src/VTS.Backend/VTS.Backend.Core.Application/Extensions/VtsDateTimeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VTS.Backend.Core.Application.Extensions
+{
+    // Conversion rule reference : https://iditect.com/guide/csharp/csharp_howto_convert_datetime.html
+    public static class VtsDateTimeExtensions
+    {
+        public static double ToUnixTimeStampInSeconds(this DateTime dateTime)
+        {
+            long unixTimestampInSeconds = new DateTimeOffset(dateTime).ToUnixTimeSeconds();
+            return unixTimestampInSeconds;
+        }
+
+        public static double ToUnixTimeStampInMiliSeconds(this DateTime dateTime)
+        {
+            long unixTimestampInMilliseconds = new DateTimeOffset(dateTime).ToUnixTimeMilliseconds();
+            return unixTimestampInMilliseconds;
+        }
+
+        public static DateTime ToUtcDateTime(this double timeStampInSeconds)
+        {
+            DateTime utcDatetime = DateTimeOffset.FromUnixTimeSeconds(timeStampInSeconds.ConvertTimeStampTolong()).UtcDateTime;
+            return utcDatetime;
+        }
+
+        public static DateTime ToDateTime(this double timeStampInSeconds)
+        {
+            DateTime localDatetime = DateTimeOffset.FromUnixTimeSeconds(timeStampInSeconds.ConvertTimeStampTolong()).LocalDateTime;
+            return localDatetime;
+        }
+
+        private static long ConvertTimeStampTolong(this double timeStampInSeconds)
+        {
+            return Convert.ToInt64(timeStampInSeconds);
+        }
+    }
+}
