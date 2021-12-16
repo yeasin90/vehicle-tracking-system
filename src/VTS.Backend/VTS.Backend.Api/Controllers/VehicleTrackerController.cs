052783050f8a015ab24f6747c4bec02e3f2adb39
Yeasin Abedin <abedinyeasin@gmail.com>
2021-12-16 18:36:20 +0600

    Added endpoint documentations using swagger

--- src/VTS.Backend/VTS.Backend.Api/Controllers/VehicleTrackerController.cs
+++ src/VTS.Backend/VTS.Backend.Api/Controllers/VehicleTrackerController.cs
@@ -18,22 +18,38 @@ namespace VTS.Backend.Api.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        ///  Register a vehicle
+        /// </summary>
+        /// <returns>Vehicle object created in database</returns>
         [HttpPost]
         [Route("device")]
-        public async Task<ActionResult<VehicleDto>> RegisterDeviceAsync(RegisterVehicleCommand command)
+        public async Task<ActionResult<VehicleDto>> RegisterDeviceAsync([FromBody]RegisterVehicleCommand command)
         {
             var result = await _mediator.Send(command);
             return Ok(result);
         }
 
+        /// <summary>
+        /// Record the position of a registered vehicle
+        /// </summary>
+        /// <returns>Vehicle position object created in database</returns>
         [HttpPost]
         [Route("device/position")]
-        public async Task<ActionResult<VehiclePositionDto>> RegisterDevicePositionAsync(RegisterPositionCommand command)
+        public async Task<ActionResult<VehiclePositionDto>> RegisterDevicePositionAsync([FromBody]RegisterPositionCommand command)
         {
             var result = await _mediator.Send(command);
             return Ok(result);
         }
 
+        /// <summary>
+        /// Retrieve the current position of a vehicle
+        /// </summary>
+        /// <remarks>
+        /// Retrieves last recorded position in database for a registered vehicle
+        /// </remarks>
+        /// <param name="VehicleId">Id of a registered vehicle</param>
+        /// <returns>Vehicle position object</returns>
         [HttpGet]
         [Route("device/{VehicleId}/position")]
         public async Task<ActionResult<VehiclePositionDto>> GetCurrentPositionAsync(GetCurrentPositionQuery query)
@@ -42,6 +58,16 @@ namespace VTS.Backend.Api.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Retrieve the positions of a vehicle
+        /// </summary>
+        /// <remarks>
+        /// Retrieve the positions of a vehicle during a certain time, in order to display their journey on a map (maps drawing is out of scope)
+        /// </remarks>
+        /// <param name="VehicleId">Id of a registered vehicle</param>
+        /// <param name="FromTimeStampInSeconds">From: time stamp in seconds</param>
+        /// <param name="ToTimeStampInSeconds">To: time stamp in seconds</param>
+        /// <returns>List of Vehicle position object</returns>
         [HttpGet]
         [Route("device/{VehicleId}/position/{FromTimeStampInSeconds}/{ToTimeStampInSeconds}")]
         public async Task<ActionResult> GetPositionsAsync(GetTimeIntervalPositionsQuery query)
--- src/VTS.Backend/VTS.Backend.Api/Startup.cs
+++ src/VTS.Backend/VTS.Backend.Api/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 using VTS.Backend.Core.Application;
 using VTS.Backend.Core.Application.Middleware;
 using VTS.Backend.Infrastructure.Persistence;
@@ -37,6 +40,10 @@ namespace VTS.Backend.Api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Vehicle tracking system API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
